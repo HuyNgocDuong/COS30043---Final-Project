ec244754f1a9e3dc8f70fafea2878be83aef50a7
HuyNgocDuong <ngochuyduong288@gmail.com>
2025-06-03 19:40:31 +1000

    Update vue.config.js

--- real-estate-app/vue.config.js
+++ real-estate-app/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-    publicPath: '/COS30043---Final-Project/real-estate-app/'
+    publicPath: './'
 })
